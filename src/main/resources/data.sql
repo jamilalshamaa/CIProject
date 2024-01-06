14579af50c1b94cb5a7c58b1363926638cb1a71f
jamilalshamaa <jamilsham3a@gmail.com>
2024-01-06 23:01:01 +0100

    Demo Data Test CRUD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+DELETE FROM student;
+INSERT INTO student (id, name, age) VALUES (1, 'Jamil', 22);
+INSERT INTO student (id, name, age) VALUES (2, 'Leo', 26);
+INSERT INTO student (id, name, age) VALUES (3, 'Like', 25);
+INSERT INTO student (id, name, age) VALUES (4, 'Joe', 21);
+INSERT INTO student (id, name, age) VALUES (5, 'Rory', 23);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS student (
+                                       id INT PRIMARY KEY,
+                                       name VARCHAR(100),
+    age INT
+    );
+create sequence IF NOT EXISTS RTDS_ADSINPUT_SEQ start with 1 increment by 1;
